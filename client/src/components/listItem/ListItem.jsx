93a7e3da175e29be0cee4e522a6e8901e4c30f47
M1chalS <michalszajner@wp.pl>
2022-11-28 23:48:06 +0100

    Trailer on hover

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,9 +1,54 @@
 import "./listItem.scss";
+import {Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined} from "@mui/icons-material";
+import {useState} from "react";
+import ReactPlayer from "react-player";
 
-export const ListItem = () => {
+export const ListItem = ({index}) => {
+
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer = "https://youtu.be/Lt-U_t2pUHI";
+
     return (
-        <div className="listItem">
-            item
+
+        <div className="listItem"
+             style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+             onMouseEnter={() => setIsHovered(true)}
+             onMouseLeave={() => setIsHovered(false)}
+        >
+            <img
+                src={"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/226DB7C90442683C5F252E9578355540E75C0643925E5ACA9EF87B5AB7340977/scale?width=1200&aspectRatio=1.78&format=jpeg"}
+                alt={"video miniature"}/>
+            {isHovered && (
+                <>
+                    <ReactPlayer className={"video"}
+                                 url={trailer}
+                                 playing={true}
+                                 width={"100%"}
+                                 height={"140px"}
+                    />
+                    <div className={"itemInfo"}>
+                        <div className={"icons"}>
+                            <PlayArrow/>
+                            <Add/>
+                            <ThumbUpAltOutlined/>
+                            <ThumbDownAltOutlined/>
+                        </div>
+                        <div className={"itemInfo--top"}>
+                            <span>2hours 44 minutes</span>
+                            <span className={"limit"}>+13</span>
+                            <span>2018</span>
+                        </div>
+                        <div className={"desc"}>
+                            Persecuti no cum no habitant oporteat neque ultricies bibendum ius. Atomorum faucibus
+                            assueverit
+                            hendrerit.
+                        </div>
+                        <div className={"genre"}>
+                            Action
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
